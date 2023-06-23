ed4f312c0ed67e2a7f8d831d94a24982f108f8c2
Tryd0g0lik <work80@mail.ru>
2023-06-23 08:54:11 +0700

    ERROR

--- webpack.config.js
+++ webpack.config.js
@@ -1,68 +1,66 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const miniCssExtractPlugin = require("mini-css-extract-plugin");
 const TsconfigPathPlugin = require("tsconfig-paths-webpack-plugin");
-// const loader = require("mini-css-extract-plugin/types/loader");
-
+require("webpack-dev-server");
 const isProduction = process.env.NODE_ENV == "production";
 
 module.exports = {
+	mode: 'none',
 	target: 'web',
-  entry: "./src/index.ts",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-  },
+	entry: "./src/index.js",
+	output: {
+		path: path.resolve(__dirname, "dist"),
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-			template: "./index.html",
-			filename: "./index.html",
-			minify: {
-				collapseWhitespace: false,
-			}
-    }),
 
-		new TsconfigPathPlugin({
-			configFile: "./tsconfig.json"
-		})
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
-  module: {
-    rules: [
+	module: {
+		rules: [
 			{
-				exclude: ["/node_modules/"],
-				test: /\.(ts|tsx)$/i,
+				test: /\.ts$/i,
 				exclude: /node_modules/,
-				use: {
-					loader: ["ts-loader", "babel-loader"],
-				},
-				options: {
-					transpileOnly: true,
-				},
+				use: "ts-loader",
 
 			},
 			{
-				test: /\.html/,
+				test: /\.js$/i,
+				exclude: /node_modules/,
+				use: [
+					{ loader: "babel-loader" },
+				],
+			},
+			{
+				test: /\.html$/,
 				use: [
 					{
-						loader: "html-loader"
+						loader: "html-loader",
 					},
 				]
 			},
 
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
+			{
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: "asset",
+			},
 
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
-  },
-  resolve: {
-		extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
-
+			// Add your rules for custom modules here
+			// Learn more about loaders from https://webpack.js.org/loaders/
+		],
 	},
-};
+
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "./src/index.html",
+			filename: "./index.html",
+			minify: {
+				collapseWhitespace: false,
+			}
+		}),
+
+		new TsconfigPathPlugin({
+			configFile: "./tsconfig.json"
+		})
+	]
+}
+		// Add your plugins here
+		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
