4515397c4e3b224c4b5427657fa154c05779abf8
Tryd0g0lik <work80@mail.ru>
2023-08-17 11:53:21 +0700

    corrected a webpack.prod.js file

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,17 +1,8 @@
 const { merge } = require('webpack-merge');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 const common = require("./webpack.config");
 
 module.exports = merge(common, {
 	mode: 'production',
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: './src/index.html',
 
-			// filename: './[name].html',
-			minify: {
-				collapseWhitespace: true,
-			},
-		}),
-	],
 });
